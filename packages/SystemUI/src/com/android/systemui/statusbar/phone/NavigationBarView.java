d42688257a23ce7377655a416680d5a88164ef1c
Daniel Koman <dankoman30@gmail.com>
2015-06-24 13:17:43 -0400

    SystemUI: fix navbar/pie icon size if icon is not a vector
    Change-Id: If72ce9ffae2c90d38ac2a8ce57daca4dcf2a5236
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/pie/PieItem.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/PieController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -534,7 +534,7 @@ public class NavigationBarView extends LinearLayout {
             if (colorize && mNavBarButtonColorMode != 3) {
                 d = ColorHelper.getColoredDrawable(d, mNavBarButtonColor);
             }
-            v.setImageDrawable(d);
+            v.setImageBitmap(ColorHelper.drawableToBitmap(d));
         }
         v.setRippleColor(mRippleColor);
         return v;
@@ -582,10 +582,8 @@ public class NavigationBarView extends LinearLayout {
             } else {
                 d = ColorHelper.getColoredDrawable(d, mNavBarButtonColor);
             }
-            v.setImageDrawable(d);
-        } else {
-            v.setImageDrawable(d);
         }
+        v.setImageBitmap(ColorHelper.drawableToBitmap(d));
         v.setRippleColor(mRippleColor);
 
         return v;
