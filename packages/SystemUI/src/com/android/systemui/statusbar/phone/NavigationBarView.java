322f1377a0a4aeeca72072cdd16f7ab9fe4811c4
Daniel Koman <dankoman30@gmail.com>
2015-07-01 19:32:53 +0200

    SystemUI: fix navbar icon too big when using custom icon
    Change-Id: Iccba11ebce965e8a5cf6c4bbf373b132ee10adb5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -465,7 +465,7 @@ public class NavigationBarView extends LinearLayout {
         }
 
         if (clickAction.startsWith("**")) {
-            v.setScaleType(KeyButtonView.ScaleType.CENTER);
+            v.setScaleType(KeyButtonView.ScaleType.CENTER_INSIDE);
         }
 
         boolean colorize = true;
@@ -505,7 +505,7 @@ public class NavigationBarView extends LinearLayout {
         KeyButtonView v = new KeyButtonView(mContext, null);
         int width = mContext.getResources().getDimensionPixelSize(R.dimen.navigation_extra_key_width);
         v.setLayoutParams(getLayoutParams(landscape, width));
-        v.setScaleType(KeyButtonView.ScaleType.CENTER);
+        v.setScaleType(KeyButtonView.ScaleType.CENTER_INSIDE);
         if (keyId == KEY_MENU_LEFT || keyId == KEY_MENU_RIGHT) {
             v.setClickAction(ActionConstants.ACTION_MENU);
             v.setLongpressAction(ActionConstants.ACTION_NULL);
