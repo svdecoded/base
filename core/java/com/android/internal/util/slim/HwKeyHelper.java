SlimRoms/frameworks_base

[1/2] Slims Hardware Key Rebinding
First thanks to CM team for the 2 years where we used their approach for our
custom hw key rebinding. This hardware key rebinding
patches did in the past a good job and we extended it prior 4.3 with a lot
Slim custom stuff.

But it is time to go new way and to do the complete
hardware key rebinding more flexible and more centralized. Especially due
that we have now all possible shorcutactions centralized in 4.4

Possible mapping are:
ACTION_HOME
ACTION_BACK
ACTION_SEARCH (in app search)
ACTION_VOICE_SEARCH
ACTION_MENU_BIG
ACTION_POWER
ACTION_NOTIFICATIONS
ACTION_QS
ACTION_RECENTS
ACTION_SCREENSHOT
ACTION_IME
ACTION_LAST_APP
ACTION_KILL
ACTION_ASSIST
ACTION_VIB
ACTION_SILENT
ACTION_VIB_SILENT
ACTION_POWER_MENU
ACTION_TORCH
ACTION_EXPANDED_DESKTOP
ACTION_THEME_SWITCH

and custom app/shorcut of course

Possible actions on all keys are:
normal tap / longpress / double tap

So to sumarize if it is in settings array enabled user can bind whatever of the above
actions to every hardware key action.

Main changes to prior solutions are:
- do not depend anymore on hardcoded integer values. We use now strings. This has
  the advantage that we can save whatever shorcut or intent URI we have into this
  value. And gives us the possibility in future without changing anything on the hw
  key rebinding code to get more options/or modify the options. Actions are holded
  like on all other of our features in ButtonsConstants.java
- All actions are peformed over our SlimActions like all other features. SlimActions
  takes care about all possible solutions the user can run into and sets the right
  open animation, keyguard handling etc etc. As well it is easier to maintain only
  one code base.
- Only observe and update the values on hardware key devices. On non hw key devices
  the complete code is non operational
- Removed in settings the not needed overlay due that we can use the fb one to
  check if it is a hw key device or not.
- Major cleanup of

core/java/com/android/internal/util/slim/HwKeyHelper.java
@@ -1,0 +1,235 @@
+/*
+ * Copyright (C) 2014 SlimRoms Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.slim;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+public class HwKeyHelper {
+
+    // These need to match the documentation/constant in
+    // core/res/res/values/config.xml
+    static final int LONG_PRESS_HOME_NOTHING = 0;
+    static final int LONG_PRESS_HOME_RECENT_SYSTEM_UI = 1;
+    static final int LONG_PRESS_HOME_ASSIST = 2;
+
+    static final int DOUBLE_TAP_HOME_NOTHING = 0;
+    static final int DOUBLE_TAP_HOME_RECENT_SYSTEM_UI = 1;
+
+    public static String getPressOnHomeBehavior(Context context, boolean getDefault) {
+        String defaultValue = ButtonsConstants.ACTION_HOME;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_HOME_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getLongPressOnHomeBehavior(Context context, boolean getDefault) {
+        String defaultValue;
+        int longPressOnHome = context.getResources().getInteger(
+                com.android.internal.R.integer.config_longPressOnHomeBehavior);
+        if (longPressOnHome == LONG_PRESS_HOME_RECENT_SYSTEM_UI) {
+            defaultValue = ButtonsConstants.ACTION_RECENTS;
+        } else if (longPressOnHome == LONG_PRESS_HOME_ASSIST){
+            defaultValue = ButtonsConstants.ACTION_SEARCH;
+        } else {
+            defaultValue = ButtonsConstants.ACTION_NULL;
+        }
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_HOME_LONG_PRESS_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getDoubleTapOnHomeBehavior(Context context, boolean getDefault) {
+        String defaultValue;
+        int doubleTapOnHome = context.getResources().getInteger(
+                com.android.internal.R.integer.config_doubleTapOnHomeBehavior);
+        if (doubleTapOnHome == DOUBLE_TAP_HOME_RECENT_SYSTEM_UI) {
+            defaultValue = ButtonsConstants.ACTION_RECENTS;
+        } else {
+            defaultValue = ButtonsConstants.ACTION_NULL;
+        }
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_HOME_DOUBLE_TAP_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getPressOnMenuBehavior(Context context, boolean getDefault) {
+        String defaultValue = ButtonsConstants.ACTION_MENU_BIG;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_MENU_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getLongPressOnMenuBehavior(Context context,
+                boolean getDefault, boolean hasAssistOrNoMenu) {
+        String defaultValue;
+        if (hasAssistOrNoMenu) {
+            defaultValue = ButtonsConstants.ACTION_NULL;
+        } else {
+            defaultValue = ButtonsConstants.ACTION_SEARCH;
+        }
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_MENU_LONG_PRESS_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getDoubleTapOnMenuBehavior(Context context, boolean getDefault) {
+        String defaultValue = ButtonsConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_MENU_DOUBLE_TAP_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getPressOnBackBehavior(Context context, boolean getDefault) {
+        String defaultValue = ButtonsConstants.ACTION_BACK;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_BACK_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getLongPressOnBackBehavior(Context context, boolean getDefault) {
+        String defaultValue = ButtonsConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_BACK_LONG_PRESS_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getDoubleTapOnBackBehavior(Context context, boolean getDefault) {
+        String defaultValue = ButtonsConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_BACK_DOUBLE_TAP_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getPressOnAssistBehavior(Context context, boolean getDefault) {
+        String defaultValue = ButtonsConstants.ACTION_SEARCH;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_ASSIST_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getLongPressOnAssistBehavior(Context context, boolean getDefault) {
+        String defaultValue = ButtonsConstants.ACTION_VOICE_SEARCH;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_ASSIST_LONG_PRESS_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getDoubleTapOnAssistBehavior(Context context, boolean getDefault) {
+        String defaultValue = ButtonsConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_ASSIST_DOUBLE_TAP_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getPressOnAppSwitchBehavior(Context context, boolean getDefault) {
+        String defaultValue = ButtonsConstants.ACTION_RECENTS;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_APP_SWITCH_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getLongPressOnAppSwitchBehavior(Context context, boolean getDefault) {
+        String defaultValue = ButtonsConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getDoubleTapOnAppSwitchBehavior(Context context, boolean getDefault) {
+        String defaultValue = ButtonsConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_APP_SWITCH_DOUBLE_TAP_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+}

svdecoded/base

Conflicts:
	core/java/android/provider/Settings.java
	core/java/com/android/internal/util/slim/Butt

core/java/com/android/internal/util/slim/HwKeyHelper.java
@@ -1,0 +1,271 @@
+/*
+ * Copyright (C) 2014-2015 SlimRoms Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util.slim;
+
+import android.content.Context;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+public class HwKeyHelper {
+
+    // These need to match the documentation/constant in
+    // core/res/res/values/config.xml
+    static final int LONG_PRESS_HOME_NOTHING = 0;
+    static final int LONG_PRESS_HOME_RECENT_SYSTEM_UI = 1;
+    static final int LONG_PRESS_HOME_ASSIST = 2;
+
+    static final int DOUBLE_TAP_HOME_NOTHING = 0;
+    static final int DOUBLE_TAP_HOME_RECENT_SYSTEM_UI = 1;
+
+    public static String getPressOnHomeBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_HOME;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_HOME_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getLongPressOnHomeBehavior(Context context, boolean getDefault) {
+        String defaultValue;
+        int longPressOnHome = context.getResources().getInteger(
+                com.android.internal.R.integer.config_longPressOnHomeBehavior);
+        if (longPressOnHome == LONG_PRESS_HOME_RECENT_SYSTEM_UI) {
+            defaultValue = ActionConstants.ACTION_RECENTS;
+        } else if (longPressOnHome == LONG_PRESS_HOME_ASSIST){
+            defaultValue = ActionConstants.ACTION_SEARCH;
+        } else {
+            defaultValue = ActionConstants.ACTION_NULL;
+        }
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_HOME_LONG_PRESS_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getDoubleTapOnHomeBehavior(Context context, boolean getDefault) {
+        String defaultValue;
+        int doubleTapOnHome = context.getResources().getInteger(
+                com.android.internal.R.integer.config_doubleTapOnHomeBehavior);
+        if (doubleTapOnHome == DOUBLE_TAP_HOME_RECENT_SYSTEM_UI) {
+            defaultValue = ActionConstants.ACTION_RECENTS;
+        } else {
+            defaultValue = ActionConstants.ACTION_NULL;
+        }
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_HOME_DOUBLE_TAP_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getPressOnMenuBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_MENU_BIG;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_MENU_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getLongPressOnMenuBehavior(Context context,
+                boolean getDefault, boolean hasAssistOrNoMenu) {
+        String defaultValue;
+        if (hasAssistOrNoMenu) {
+            defaultValue = ActionConstants.ACTION_NULL;
+        } else {
+            defaultValue = ActionConstants.ACTION_SEARCH;
+        }
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_MENU_LONG_PRESS_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getDoubleTapOnMenuBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_MENU_DOUBLE_TAP_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getPressOnBackBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_BACK;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_BACK_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getLongPressOnBackBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_BACK_LONG_PRESS_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getDoubleTapOnBackBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_BACK_DOUBLE_TAP_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getPressOnAssistBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_SEARCH;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_ASSIST_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getLongPressOnAssistBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_VOICE_SEARCH;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_ASSIST_LONG_PRESS_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getDoubleTapOnAssistBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_ASSIST_DOUBLE_TAP_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getPressOnAppSwitchBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_RECENTS;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_APP_SWITCH_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getLongPressOnAppSwitchBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_APP_SWITCH_LONG_PRESS_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getDoubleTapOnAppSwitchBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_APP_SWITCH_DOUBLE_TAP_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getPressOnCameraBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_CAMERA;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_CAMERA_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getLongPressOnCameraBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_CAMERA_LONG_PRESS_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+
+    public static String getDoubleTapOnCameraBehavior(Context context, boolean getDefault) {
+        String defaultValue = ActionConstants.ACTION_NULL;
+        if (getDefault) {
+            return defaultValue;
+        }
+        String value = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.KEY_CAMERA_DOUBLE_TAP_ACTION,
+                UserHandle.USER_CURRENT);
+        return value == null ? defaultValue : value;
+    }
+}